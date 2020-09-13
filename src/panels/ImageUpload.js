bebb119b4fdbe828d990d5a193bbc5f0a10fd792
gvencadze <humblebumble17@gmail.com>
2020-09-13 22:48:39 +0300

    Модуль ImageUpload добавлен вместо <File> в monthlyDonate.js & targetDonate.js

--- src/panels/ImageUpload.js
+++ src/panels/ImageUpload.js
@@ -1,34 +1,34 @@
 import React, {useState, useEffect} from 'react'
-import {File, Banner} from "@vkontakte/vkui";
+import {File} from "@vkontakte/vkui";
 import {Icon28PictureOutline} from "@vkontakte/icons";
 
 const ImageUpload = () => {
-    const [selectedFile, setSelectedFile] = useState()
-    const [preview, setPreview] = useState()
+    const [selectedFile, setSelectedFile] = useState();
+    const [preview, setPreview] = useState();
 
     // create a preview as a side effect, whenever selected file is changed
     useEffect(() => {
         if (!selectedFile) {
-            setPreview(undefined)
+            setPreview(undefined);
             return
         }
 
-        const objectUrl = URL.createObjectURL(selectedFile)
-        setPreview(objectUrl)
+        const objectUrl = URL.createObjectURL(selectedFile);
+        setPreview(objectUrl);
 
         // free memory when ever this component is unmounted
         return () => URL.revokeObjectURL(objectUrl)
-    }, [selectedFile])
+    }, [selectedFile]);
 
     const onSelectFile = e => {
         if (!e.target.files || e.target.files.length === 0) {
-            setSelectedFile(undefined)
+            setSelectedFile(undefined);
             return
         }
 
         // I've kept this example simple by using the first image instead of multiple
         setSelectedFile(e.target.files[0])
-    }
+    };
 
     return (
         <div>
@@ -39,6 +39,6 @@ const ImageUpload = () => {
             }
         </div>
     )
-}
+};
 
 export default ImageUpload
